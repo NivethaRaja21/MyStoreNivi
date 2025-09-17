f05377bb7b4f64ea79f70bb25ea8d39ff13b763d
herenivi-21 <nivethadarshan79@gmail.com>
2025-09-17 09:25:16 +0530

    first commit

---
+++ src/main/java/com/mystorenivi/mystore1/Mystore1Application.java
@@ -0,0 +1,13 @@
+package com.mystorenivi.mystore1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mystore1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mystore1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mystorenivi/mystore1/Mystore1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystorenivi.mystore1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mystore1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
